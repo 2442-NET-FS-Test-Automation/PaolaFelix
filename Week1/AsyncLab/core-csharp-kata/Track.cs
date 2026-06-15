46ebb91f7fb960b336a821045277f5cd37afcef4
Paola Felix <pfelixtorres@gmail.com>
2026-06-14 22:14:24 -0600

    added abstract class Track with encapsulation

---
+++ Week1/AsyncLab/core-csharp-kata/Track.cs
@@ -0,0 +1,38 @@
+namespace PlaylistKata;
+
+public abstract class Track
+{
+    public string Title {get; set;}
+    public string Artist {get; set;}
+    public DateTime RealeaseDate {get; set;}
+    public bool IsLiked {get; set;}
+    private int _duration;
+
+    public int Duration
+    {
+        get {return _duration;}
+        set
+        {
+            if (value < 0)
+            {
+                _duration = 0;
+            }
+            else
+            {
+                _duration = value;
+            }
+        }
+    }
+
+    public Track(string title, string artist, int duration, DateTime realeaseDate)
+    {
+        Title = title;
+        Artist = artist;
+        Duration = duration;
+        RealeaseDate = realeaseDate;
+        IsLiked = false;
+    }
+
+    public static int TracksAdded {get; private set;}
+    public abstract void Play();
+}
