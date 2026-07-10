de75e6b112dfcaf523b6cc7d35ff093dac597d04
Paola Felix <pfelixtorres@gmail.com>
2026-07-10 15:11:06 -0600

    deleting folder 2.0

--- Week4/dsa-demo/DsaThreading/obj/Debug/net10.0/DsaThreading.AssemblyInfo.cs
+++ Week4/dsa-demo/DsaThreading/obj/Debug/net10.0/DsaThreading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DsaThreading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3291c5b80b89fd388da35b131f32a60d0a460282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0043ce26733acfcf6350550ed790e1bbbf82697e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DsaThreading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DsaThreading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
